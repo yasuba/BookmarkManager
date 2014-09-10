f07f85d92cf01252ff5e267e705e6bdfbbbdde7c
Maya Driver <mayadrivermd@gmail.com>
2014-09-10 17:30:01 +0100

    added server file

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source "https://rubygems.org"
-
+gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 
 group :development, :test do
 	gem "rspec"
 	gem 'database_cleaner'
+	gem 'capybara'
 end
---
+++ server.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'data_mapper'
+env = ENV["RACK_ENV"] || "development"
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
+
+get '/' do
+	@links = Link.all
+	erb :index
+end
