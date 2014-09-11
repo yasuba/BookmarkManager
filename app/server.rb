6fd0aa2efc49689aae20fe9d80471bc77b3b42df
Maya Driver <mayadrivermd@gmail.com>
2014-09-11 11:20:00 +0100

    added user accounts

---
+++ app/server.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'data_mapper'
+require './lib/link'
+require './lib/tag'
+require './lib/user'
+require_relative 'helpers/application'
+require_relative 'data_mappers_setup'
+
+enable :sessions
+set :session_secret, 'super secret'
+
+get '/' do
+	@links = Link.all
+	erb :index
+end
+
+post '/links' do
+	url = params['url']
+	title = params['title']
+	tags = params["tags"].split(" ").map do |tag|
+		Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
+	redirect to ('/')
+end
+
+get '/tags/:text' do
+	tag = Tag.first(:text => params[:text])
+	@links = tag ? tag.links : []
+	erb :index
+end
+
+get '/users/new' do
+	erb :"users/new"
+end
+
+post '/users' do
+	user = User.create(:email => params[:email],
+				:password => params[:password])
+	session[:user_id] = user.id
+	redirect to('/')
+end
