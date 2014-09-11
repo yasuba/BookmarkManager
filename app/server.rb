180ef2fa527a478ee4265f000aacca64a5021d4c
Maya Driver <mayadrivermd@gmail.com>
2014-09-11 21:17:02 +0100

    allowing users to sign in

--- app/server.rb
+++ app/server.rb
@@ -58,4 +58,21 @@ class BookmarkManager < Sinatra::Base
 			erb :"users/new"
 		end
 	end
+
+	get '/sessions/new' do
+		erb :"sessions/new"
+	end
+
+	post '/sessions' do
+		email, password = params[:email], params[:password]
+		user = User.authenticate(email, password)
+		if user
+			session[:user_id] = user.id
+			redirect to('/')
+		else
+			flash[:errors] = ["The email or password is incorrect"]
+			erb :"sessions/new"
+		end
+	end
+
 end
