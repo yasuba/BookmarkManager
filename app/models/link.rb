032e9f1f687dc1d322073b064c219dfa4953f0b1
Maya Driver <mayadrivermd@gmail.com>
2014-09-11 22:40:11 +0100

    allowing users to sign out

---
+++ app/models/link.rb
@@ -0,0 +1,15 @@
+# This class corresponds to a table in the database
+# We can use it to manipulate the data
+class Link
+
+	# this makes the instances of this class Datamapper resources	
+	include DataMapper::Resource
+
+	has n, :tags, :through => Resource
+	
+	# This block describes what resources our model will have
+	property :id, 		Serial
+	property :title, 	String
+	property :url, 		String
+
+end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper'
+require_relative 'helpers/session'
+
+include SessionHelpers
+
+feature "User signs in" do
+
+		before(:each) do
+			User.create(:email => "test@test.com",
+						:password => 'test',
+						:password_confirmation => 'test')
+		end
+
+		scenario "with correct credentials" do
+			visit '/'
+			expect(page).not_to have_content("Welcome, test@test.com")
+			sign_in('test@test.com', 'test')
+			expect(page).to have_content("Welcome, test@test.com")
+		end
+
+		scenario "with incorrect credentials" do
+			visit '/'
+			expect(page).not_to have_content("Welcome, test@test.com")
+			sign_in('test@test.com', 'wrong')
+			expect(page).not_to have_content("Welcome, test@test.com")
+		end
+
+end
+
+feature 'User signs out' do
+
+	before(:each) do
+		User.create(:email => "test@test.com",
+					:password => 'test',
+					:password_confirmation => 'test')
+	end
+
+	scenario 'while being signed in' do
+		sign_in('test@test.com', 'test')
+		click_button "Sign out"
+		expect(page).to have_content("Goodbye!")
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+end
 
 feature "User signs up" do
 
@@ -11,55 +55,13 @@ feature "User signs up" do
 	scenario "with a password that doesn't match" do
 		expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
 		expect(current_path).to eq('/users')
-		expect(page).to have_content("Password does not match the confirmation")
+		expect(page).to have_content("The email or password is incorrect")
 	end
 
 	scenario "with an email that is already registered" do
 		expect { sign_up }.to change(User, :count).by(1)
 		expect { sign_up }.to change(User, :count).by(0)
 		expect(page).to have_content("This email is already taken")
-	end
-
-	def sign_up(email = "alice@example.com",
-				password = "oranges!",
-				password_confirmation = "oranges!")
-		visit '/users/new'
-		expect(page.status_code).to eq(200)
-		fill_in :email, :with => email
-		fill_in :password, :with => password
-		fill_in :password_confirmation, :with => password_confirmation
-		click_button "Sign up"
-	end
-
-end
-
-feature "User signs in" do
-
-	before(:each) do
-		User.create(:email => "test@test.com",
-					:password => 'test',
-					:password_confirmation => 'test')
-	end
-
-	scenario "with correct credentials" do
-		visit '/'
-		expect(page).not_to have_content("Welcome, test@test.com")
-		sign_in('test@test.com', 'test')
-		expect(page).to have_content("Welcome, test@test.com")
-	end
-
-	scenario "with incorrect credentials" do
-		visit '/'
-		expect(page).not_to have_content("Welcome, test@test.com")
-		sign_in('test@test.com', 'wrong')
-		expect(page).not_to have_content("Welcome, test@test.com")
-	end
-
-	def sign_in(email, password)
-		visit '/sessions/new'
-		fill_in 'email', :with => email
-		fill_in 'password', :with => password
-		click_button 'Sign in'
 	end
 
 end
